0601106916dca3bdb13be2384854b7e68c8b70ef
ramadbp <rdan60404@gmail.com>
2025-04-15 14:16:15 +0700

    first commit

--- src/service/api.js
+++ src/service/api.js
@@ -1,4 +1,4 @@
 export async function getUsers() {
-  const response = await fetch("https://presensi-vue.irfannuddin35272.workers.dev/api/users");
+  const response = await fetch("https://vue-rama.rdan60404.workers.dev/api/produks");
   return await response.json();
 }
