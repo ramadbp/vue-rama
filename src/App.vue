51c75da057048bcb843760fb09c8550d4b946391
ramadbp <rdan60404@gmail.com>
2025-04-15 14:20:50 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, onMounted } from "vue";
-import { getUsers } from "./services/api";
+import { getUsers } from "./service/api";
 
 const users = ref([]);
 
@@ -13,7 +13,7 @@ onMounted(async () => {
   <div>
     <h1>Users</h1>
     <ul>
-      <li v-for="produk in users.results" :key="produk.id">{{ user.nama }}</li>
+      <li v-for="produk in users.results" :key="produk.id">{{ produk.nama }}</li>
     </ul>
   </div>
 </template>
